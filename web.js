ea546198c34a4058640104daaf603718a4c9d02b
Amy Grude <agrude@gmail.com>
2013-07-13 22:05:49 +0000

    added index and updated js

--- web.js
+++ web.js
@@ -6,6 +6,14 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
