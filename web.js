760dc32f58d2e123fda5cc5be6213b6cff8b0d4e
Amy Grude <agrude@gmail.com>
2013-07-13 22:39:49 +0000

    6

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+var fs = require('fs');
 
-app.get('/', function(request, response) {
-  var fs = require('fs');
-  var content = fs.readFileSync('index.html','utf8');
+var data = fs.readFileSync("index.html","utf-8");
+
+var app = express.createServer(express.logger());
+app.get('/', function(request,response){
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function(){
   console.log("Listening on " + port);
 });
