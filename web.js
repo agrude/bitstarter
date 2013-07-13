211201b2af19c5eac83c3cdff17f080907121041
Amy Grude <agrude@gmail.com>
2013-07-13 22:21:02 +0000

    5

--- web.js
+++ web.js
@@ -6,23 +6,6 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-var content;
-// First I want to read the file
-fs.readFile('./index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-
-    // Invoke the next step here however you like
-    console.log(content);   // Put all of the code here (not the best solution)
-    processFile();          // Or put the next step in a function and invoke it
-});
-
-function processFile() {
-    console.log(content);
-}
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
